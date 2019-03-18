cb8ac43a1337234f5909552b85865865eb1e7365
Cynthia <wangxi.cynthia1@gmail.com>
2019-03-18 15:35:22 -0600

    Added variation 2.

--- main.js
+++ main.js
@@ -7,16 +7,11 @@ window.onload = function(){
     setupVis3();
 };
 
-/**
- * Global variables
- */
-var _vis1;       // The visualization
-
 var SpiralPlot = function(){
     this.data;  // the data subset to present
-    this.width = 800;
-    this.height = 800;
-    this.margin = {top:50, bottom:50, left:50, right:50};
+    this.width = WIDTH;
+    this.height = HEIGHT;
+    this.margin = MARGIN;
     this.start = 2.2;
     this.end = 0;
     this.spiralNum = 3;
@@ -354,20 +349,330 @@ var SpiralPlot = function(){
 function setupVis1() {
     _vis11 = new SpiralPlot();
     _vis11.svgContainer = d3.select("#v11_svg");
-
-    loadData("worldwide-literacy.csv");
-}
-
-function loadData(path) {
-    d3.csv(path).then(function(data){
+    // loadData("worldwide-literacy.csv");
+    d3.csv("worldwide-literacy.csv").then(function(data){
         _vis11.data = data;
         console.log(_vis11.data);
         _vis11.setSpiral();
     })
 }
 
-function setupVis2() {
 
+/**
+ * Global variables
+ */
+const WIDTH = 800;
+const HEIGHT = 800;
+const MARGIN = {top:50, bottom:50, left:50, right:50};
+const start = 2.2,
+    end = 0,
+    spiralNum = 3,
+    minRadius = 60;
+
+theta = function(r) {
+    return spiralNum * Math.PI * r;
+};
+
+function setSpiral2(data){  // variation 2
+    // set svg attributes
+    var svg = d3.select('#v12_svg')
+        .attr("width", WIDTH + MARGIN.right + MARGIN.left)
+        .attr("height", HEIGHT + MARGIN.right + MARGIN.left)
+        .append("g")
+        .attr('id', "g-vis2")
+        .attr("transform", "translate(500,470)");
+
+    // draw the plain spiral
+    var r = d3.min([WIDTH, HEIGHT])/2-minRadius;
+    var radius = d3.scaleLinear()
+        .domain([start, end])
+        .range([minRadius, r]);
+
+    var spiral = d3.radialLine()
+        .curve(d3.curveCardinal)
+        .angle(theta)
+        .radius(radius);
+
+    points = d3.range(start, end + 0.001, (end-start)/1000);
+
+    var path = svg.append("path")
+        .datum(points)
+        .attr("id", "spiral2")
+        .attr("d", spiral)
+        .style("fill", "none")
+        .style("stroke", "steelblue");
+
+    // set scales
+    var spiralLength = path.node().getTotalLength(),
+        N = data.length-1,
+        barWidth = (spiralLength/N) - 1;
+
+    var xScale = d3.scaleBand()
+        .domain(data.map(function(d){
+            return d['Country/Region'];
+        }))
+        .range([0,spiralLength]);
+
+    var yScale = d3.scaleLinear()
+        .domain([0,15])
+        .range([2, 70]);
+
+    var colorScale = d3.scaleQuantize()
+        .domain([d3.min(data, function(d){
+            if(d['Share of education in governmental expenditure (%)'] !== "")
+                return +d['Share of education in governmental expenditure (%)'];
+        }), d3.max(data, function(d){
+            return +d['Share of education in governmental expenditure (%)'];
+        })])
+        .range(colorbrewer.PuBuGn[9]);
+
+
+    // bind data to the spiral
+    svg.selectAll("rect")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("class", "vis2_rect")
+        .attr("x", function(d, i){
+            var linePer = xScale(d['Country/Region']),
+                posOnLine = path.node().getPointAtLength(linePer),
+                angleOnLine = path.node().getPointAtLength(linePer - barWidth);
+
+            d.linePer = linePer;
+            d.x = posOnLine.x;
+            d.y = posOnLine.y;
+            d.a = (Math.atan2(angleOnLine.y, angleOnLine.x) * 180/Math.PI) - 90;
+
+            return d.x;
+        })
+        .attr("y", function(d){
+            /* Variation 2: change the rectangle to point inwards*/
+            if(d['Mean years of schooling'] == ""){
+                return d.y - 20;
+            }
+            else return d.y;
+        })
+        .attr("width", function(d){
+            return barWidth;
+        })
+        .attr("height", function(d){
+            /* Variation 2: change the rectangle to point inwards*/
+            if(d['Mean years of schooling'] == ""){
+                return 20;
+            }
+            else return yScale(d['Mean years of schooling']);
+        })
+        .style("fill", function(d){
+            // return d3.interpolateSpectral(colorScale(d['Share of education in governmental expenditure (%)']));
+            if(d['Share of education in governmental expenditure (%)'] !== "")
+                return colorScale(d['Share of education in governmental expenditure (%)']);
+            else return "#666666";
+        })
+        .style("stroke", "grey")
+        .style("stroke-width", "1px")
+        .attr("transform", function(d){
+            return `rotate(${d.a},${d.x},${d.y})`;
+        });
+
+    var cnt = 0;
+    // Add text
+    svg.selectAll("text")
+        .data(data)
+        .enter()
+        .append("text")
+        .attr("dy", -25)
+        .style("text-anchor", "start")
+        .style("font", "10px arial")
+        .append("textPath")
+        // only add for the first of each month
+        .filter(function(d){
+            if(cnt === 0){
+                d.labelFlg = true;
+            }else d.labelFlg = false;
+            if(cnt++ % 18 === 0 || cnt === 213)return true;
+            return false;
+        })
+        .text(function(d){
+            if(d.labelFlg === true){
+                return "Literacy rate";
+            }
+            else return (d['Literacy rate(%)'] + "%");
+        })
+        .attr("font-weight", function(d){
+            if(d.labelFlg === true){
+                return "bold";
+            }
+            else return "normal";
+        })
+        .attr("font-size", function(d){
+            if(d.labelFlg === true){
+                return "12px";
+            }
+            else return "10px";
+        })
+        // place text along spiral
+        .attr("xlink:href", "#spiral")
+        .style("fill", "dark-grey")
+        .attr("startOffset", function(d){
+            return ((d.linePer / spiralLength) * 100) + "%";
+        });
+
+    var x0 = 500,
+        y0 = -250;
+    var legend1 = [];
+    var min_val = d3.min(data, function(d){
+            if(d['Share of education in governmental expenditure (%)'] !== "")
+                return +d['Share of education in governmental expenditure (%)'];
+        }),
+        max_val = d3.max(data, function(d){
+            return +d['Share of education in governmental expenditure (%)'];
+        });
+
+    for(let i=0; i<9; i++){
+        legend1.push({
+            value: i,
+            pos: [x0, y0+i*30],
+            t: Math.round(i * (max_val - min_val)/9)+ "~" +
+                Math.round((i+1) * (max_val - min_val)/9)
+        });
+    }
+    legend1.push({value:-1, pos:[x0, y0+270], t:"Not Available"});
+    //legend group
+    d3.select("#v12_svg")
+        .append("g")
+        .attr("class", "legend21")
+        .attr("transform", "translate(" + 500 + "," + 450 +")");
+
+    d3.select('.legend21')
+        .selectAll("rect")
+        .data(legend1)
+        .enter()
+        .append("rect")
+        .attr("x", function(d){
+            return d.pos[0];
+        })
+        .attr("y", function(d){
+            return d.pos[1];
+        })
+        .attr("height", 20)
+        .attr("width", 30)
+        .style("fill", function(d){
+            if(d.value === -1){
+                return "#666666";
+            }
+            else return colorbrewer.PuBuGn[9][d.value];
+        })
+        .style("stroke", "grey")
+        .style("stroke-width", "1px");
+
+    d3.select('.legend21')
+        .selectAll("text")
+        .data(legend1)
+        .enter()
+        .append("text")
+        .attr("x", function(d){
+            return d.pos[0] + 40;
+        })
+        .attr("y", function(d){
+            return d.pos[1] + 15;
+        })
+        .text(function(d){
+            return d.t + " %";
+        });
+
+    d3.select('.legend21')
+        .append("text")
+        .attr("x", x0-20)
+        .attr("y", y0-40)
+        .text("Share of education in")
+        .attr("font-weight", "bold");
+    d3.select('.legend21')
+        .append("text")
+        .attr("x", x0-20)
+        .attr("y", y0-20)
+        .text("government expenditure")
+        .attr("font-weight", "bold");
+
+
+    // draw axis
+    var yAxisScale = d3.scaleLinear()
+        .domain([15,0])
+        .range([0, 70]);
+    var yAxis = d3.axisLeft(yAxisScale)
+        .tickValues([0,5,10,15]);
+
+    var legend22 = d3.select("#v12_svg")
+        .append("g")
+        .attr("class", "axis2")
+        .attr("transform", "translate(" + 505 + "," + 57 +")")
+        .style("dominant-baseline", "central")
+        .call(yAxis)
+        .attr("stroke", "black")
+        .attr("fill", "grey");
+
+    legend22.select("path").attr("marker-end", "url(#arrowhead)");
+
+    var x1 = -20,
+        y1 = y0-150;
+    d3.select('.legend21')
+        .append("text")
+        .attr("x", x1)
+        .attr("y", y1)
+        .text("Mean year of schooling (years)")
+        .attr("font-weight", "bold");
+    d3.select('.legend21')
+        .append("text")
+        .attr("x", x1-70)
+        .attr("y", y1 + 100)
+        .style("font-size", 12)
+        .text("Not Available");
+
+
+    // Interaction
+    var popUp = d3.select(".tooltip");
+
+    d3.select("#g-vis2")
+        .selectAll(".vis2_rect")
+        .on('mouseover', function(d){
+            popUp.select('.country').html("Country/Region: <b>" + d['Country/Region'] + "</b>");
+            popUp.select('.literacy').html("Literacy rate: <b>" + d['Literacy rate(%)'] + "%</b>");
+            if(d['Mean years of schooling'] === "") popUp.select('.schooling').html("Mean Year of Schooling: <b>Not Available</b>");
+            else popUp.select('.schooling').html("Mean Year of Schooling: <b>" + d['Mean years of schooling'] + " years</b>");
+            if(d['Share of education in governmental expenditure (%)'] === "")popUp.select('.expenditure').html("Share of education in governmental expenditure: <b>Not Available</b>");
+            else popUp.select('.expenditure').html("Share of education in governmental expenditure: <b>" + d['Share of education in governmental expenditure (%)'] + "%</b>");
+
+            d3.select(this)
+                .style("fill", "#FFFFFF")
+                .style("stroke", "#000000")
+                .style("stroke-width", "2px");
+
+            popUp.style('display', 'block');
+            popUp.style('opacity', 2);
+        })
+        .on('mousemove', function(d){
+            popUp.style('top', (d3.event.layerY + 10) + 'px')
+                .style('left', (d3.event.layerX - 25) + 'px');
+        })
+        .on('mouseout', function(d){
+            d3.select("#g-vis2")
+                .selectAll(".vis2_rect")
+                .style("fill", function(d){
+                    if(d['Share of education in governmental expenditure (%)'] !== "")
+                        return colorScale(d['Share of education in governmental expenditure (%)']);
+                    else return "#666666";
+                })
+                .style("stroke", "grey")
+                .style("stroke-width", "1px");
+
+            popUp.style('display', 'none');
+            popUp.style('opacity', 0);
+        });
+}
+
+function setupVis2() {
+    d3.csv("worldwide-literacy.csv").then(function(data){
+        setSpiral2(data);
+    })
 }
 
 
